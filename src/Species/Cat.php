3945bc1e43c1003224e2cfb753a25f7e4543023c
Andrzej Sternal <andrzej.sternal@webchili.pl>
2018-06-14 03:27:09 +0200

    namespace change

---
+++ src/Species/Cat.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrzej
+ * Date: 2018-06-13
+ * Time: 13:41
+ */
+
+namespace Sofcik\OopAnimals\Species;
+
+
+class Cat extends \Sofcik\OopAnimals\SpeciesAbstract
+{
+    /**
+     * @var string
+     */
+    protected $name = 'Cat';
+
+    /**
+     * @var string
+     */
+    protected $sound = 'Miau Miau';
+}
---
+++ src/Species/Dog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrzej
+ * Date: 2018-06-13
+ * Time: 13:39
+ */
+
+namespace Sofcik\OopAnimals\Species;
+
+class Dog extends \Sofcik\OopAnimals\SpeciesAbstract
+{
+    /**
+     * @var string
+     */
+    protected $name = 'Dog';
+
+    /**
+     * @var string
+     */
+    protected $sound = 'Hau Hau';
+}
